e9e384f24ffa1e6d48c17c233036a15c515cbc4d
François Samin <francois.samin@corp.ovh.com>
2017-03-14 17:54:36 +0100

    fix (assertions): ShouldContainSubstring

--- assertion.go
+++ assertion.go
@@ -168,13 +168,13 @@ var assertMap = map[string]func(actual interface{}, expected ...interface{}) str
 
 // ShouldContainSubstring receives exactly more than 2 string parameters and ensures that the first contains the second as a substring.
 func ShouldContainSubstring(actual interface{}, expected ...interface{}) string {
-	var arg string
 	if len(expected) == 1 {
 		return assertions.ShouldContainSubstring(actual, expected...)
 	}
 
+	var arg string
 	for _, e := range expected {
-		arg = fmt.Sprintf("%s %v", arg, e)
+		arg += fmt.Sprintf("%v ", e)
 	}
-	return assertions.ShouldContainSubstring(actual, arg)
+	return assertions.ShouldContainSubstring(actual, strings.TrimSpace(arg))
 }
