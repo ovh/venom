7a0b83f412bcc4754d43708f721953dd87ae8012
Steven Guiheux <steven.guiheux@corp.ovh.com>
2017-04-07 16:19:21 +0200

    [auto] bump version to v0.0.6

--- venom.go
+++ venom.go
@@ -15,7 +15,7 @@ import (
 // Version of Venom
 // One Line for this, used by release.sh script
 // Keep "const Version on one line"
-const Version = "0.0.5"
+const Version = "0.0.6"
 
 // PrintFunc used by venom to print output
 var PrintFunc = fmt.Printf
