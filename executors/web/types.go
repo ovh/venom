82075fceb2b8c93abfcf9ba24640fd05bdc53212
Steven Guiheux <steven.guiheux@corp.ovh.com>
2017-06-19 11:00:17 +0200

    feat (web): add special key input

--- executors/web/types.go
+++ executors/web/types.go
@@ -2,16 +2,17 @@ package web
 
 // Action represents what can be done with web executor
 type Action struct {
-	Click    *Click   `yaml:"click,omitempty"`
-	Fill     []Fill   `yaml:"fill,omitempty"`
-	Find     string   `yaml:"find,omitempty"`
+	Click    *Click    `yaml:"click,omitempty"`
+	Fill     []Fill    `yaml:"fill,omitempty"`
+	Find     string    `yaml:"find,omitempty"`
 	Navigate *Navigate `yaml:"navigate,omitempty"`
 }
 
 // Fill represents informations needed to fill input/textarea
 type Fill struct {
-	Find string `yaml:"find,omitempty"`
-	Text string `yaml:"text,omitempty"`
+	Find string  `yaml:"find,omitempty"`
+	Text string  `yaml:"text,omitempty"`
+	Key  *string `yaml:"key,omitempty"`
 }
 
 type Click struct {
