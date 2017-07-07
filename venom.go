5bfeaef57ea5c57fd00214bb186bd7dc6f705fad
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-07-07 17:27:54 +0200

    [auto] bump version to v0.10.0

--- venom.go
+++ venom.go
@@ -15,7 +15,7 @@ import (
 // Version of Venom
 // One Line for this, used by release.sh script
 // Keep "const Version on one line"
-const Version = "0.0.9"
+const Version = "0.10.0"
 
 // PrintFunc used by venom to print output
 var PrintFunc = fmt.Printf
