932536be6045ad0df813c6ad41071b2bfff634ad
Ludovic Lamarche <ludovic.lamarche@corp.ovh.com>
2017-07-07 11:55:12 +0200

    add test executor for ovh api
    Signed-off-by: Ludovic Lamarche <ludovic.lamarche@corp.ovh.com>

--- context/default/context.go
+++ context/default/context.go
@@ -1,6 +1,12 @@
 package defaultctx
 
-import "github.com/runabove/venom"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/runabove/venom"
+)
 
 // Context Type name
 const Name = "default"
@@ -20,6 +26,7 @@ type DefaultTestCaseContext struct {
 
 // Init Initialize the context
 func (tcc *DefaultTestCaseContext) Init() error {
+	tcc.datas = tcc.TestCase.Context
 	return nil
 }
 
@@ -27,3 +34,97 @@ func (tcc *DefaultTestCaseContext) Init() error {
 func (tcc *DefaultTestCaseContext) Close() error {
 	return nil
 }
+
+func (tcc *DefaultTestCaseContext) GetString(key string) (string, error) {
+	if tcc.datas[key] == nil {
+		return "", NotFound(key)
+	}
+
+	if result, ok := tcc.datas[key].(string); !ok {
+		return "", errors.New(fmt.Sprintf("arguments %s invalid", key))
+	} else {
+		return result, nil
+	}
+}
+
+func (tcc *DefaultTestCaseContext) GetFloat(key string) (float64, error) {
+	if tcc.datas[key] == nil {
+		return 0, NotFound(key)
+	}
+
+	if result, ok := tcc.datas[key].(float64); !ok {
+		return 0, errors.New(fmt.Sprintf("arguments %s invalid", key))
+	} else {
+		return result, nil
+	}
+}
+
+func (tcc *DefaultTestCaseContext) GetInt(key string) (int, error) {
+	res, err := tcc.GetFloat(key)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(res), nil
+}
+
+func (tcc *DefaultTestCaseContext) GetBool(key string) (bool, error) {
+	if tcc.datas[key] == nil {
+		return false, NotFound(key)
+	}
+
+	if result, ok := tcc.datas[key].(bool); !ok {
+		return false, errors.New(fmt.Sprintf("arguments %s invalid", key))
+	} else {
+		return result, nil
+	}
+}
+
+func (tcc *DefaultTestCaseContext) GetStringSlice(key string) ([]string, error) {
+	if tcc.datas[key] == nil {
+		return nil, NotFound(key)
+	}
+
+	stringSlice, ok := tcc.datas[key].([]string)
+	if ok {
+		return stringSlice, nil
+	}
+
+	slice, ok := tcc.datas[key].([]interface{})
+	if !ok {
+		return nil, errors.New(fmt.Sprintf("arguments %s invalid", key))
+	}
+
+	res := make([]string, len(slice))
+
+	for k, v := range slice {
+		s, ok := v.(string)
+		if !ok {
+			return nil, errors.New("cannot cast to string")
+		}
+
+		res[k] = s
+	}
+
+	return res, nil
+}
+
+func (tcc *DefaultTestCaseContext) GetComplex(key string, arg interface{}) error {
+	if tcc.datas[key] == nil {
+		return NotFound(key)
+	}
+
+	val, err := json.Marshal(tcc.datas[key])
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(val, arg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// NotFound is error returned when trying to get missing argument
+func NotFound(key string) error { return fmt.Errorf("missing context argument '%s'", key) }
