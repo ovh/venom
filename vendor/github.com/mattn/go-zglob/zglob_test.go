518a934b27782bbb30dbb21141a414714b610e96
Sean Chittenden <sean@chittenden.org>
2018-01-11 12:23:31 -0800

    Update github.com/mattn/go-zglob

--- vendor/github.com/mattn/go-zglob/zglob_test.go
+++ vendor/github.com/mattn/go-zglob/zglob_test.go
@@ -29,6 +29,7 @@ var testGlobs = []testZGlob{
 	{`f*o/**`, []string{`foo/bar`, `foo/baz`}, nil},
 	{`*oo/**`, []string{`foo/bar`, `foo/baz`, `hoo/bar`}, nil},
 	{`*oo/b*`, []string{`foo/bar`, `foo/baz`, `hoo/bar`}, nil},
+	{`*oo/bar`, []string{`foo/bar`, `hoo/bar`}, nil},
 	{`*oo/*z`, []string{`foo/baz`}, nil},
 	{`foo/**/*`, []string{`foo/bar`, `foo/bar/baz`, `foo/bar/baz.txt`, `foo/bar/baz/noo.txt`, `foo/baz`}, nil},
 	{`*oo/**/*`, []string{`foo/bar`, `foo/bar/baz`, `foo/bar/baz.txt`, `foo/bar/baz/noo.txt`, `foo/baz`, `hoo/bar`}, nil},
@@ -37,6 +38,7 @@ var testGlobs = []testZGlob{
 	{`*oo/**/*.txt`, []string{`foo/bar/baz.txt`, `foo/bar/baz/noo.txt`}, nil},
 	{`doo`, nil, os.ErrNotExist},
 	{`./f*`, []string{`foo`}, nil},
+	{`**/bar/**/*.txt`, []string{`foo/bar/baz.txt`, `foo/bar/baz/noo.txt`}, nil},
 }
 
 func setup(t *testing.T) string {
@@ -134,5 +136,41 @@ func TestMatch(t *testing.T) {
 				t.Errorf("%q should match with %q", f, test.pattern)
 			}
 		}
+	}
+}
+
+func TestFollowSymlinks(t *testing.T) {
+	tmpdir, err := ioutil.TempDir("", "zglob")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os.MkdirAll(filepath.Join(tmpdir, "foo"), 0755)
+	ioutil.WriteFile(filepath.Join(tmpdir, "foo/baz.txt"), []byte{}, 0644)
+	defer os.RemoveAll(tmpdir)
+
+	err = os.Symlink(filepath.Join(tmpdir, "foo"), filepath.Join(tmpdir, "bar"))
+	if err != nil {
+		t.Skip(err.Error())
+	}
+
+	curdir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.Chdir(tmpdir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Chdir(curdir)
+
+	got, err := GlobFollowSymlinks("**/*")
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := []string{"foo", "foo/baz.txt", "bar/baz.txt"}
+
+	if !check(expected, got) {
+		t.Errorf(`zglob failed: expected %v but got %v`, expected, got)
 	}
 }
