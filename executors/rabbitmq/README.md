11816b2aa01815ab2a2933250c1dc93166a3af6c
orgrimarr <me@orgrimarr.fr>
2019-05-17 11:19:09 +0200

    add message headers

--- executors/rabbitmq/README.md
+++ executors/rabbitmq/README.md
@@ -26,16 +26,117 @@ In your yaml file, you can use:
   - messageLimit optional (default 1)
 
   # For publisher only
-  - durable optional      (true or false) (default alse)
-  - contentType optional  
-  - persistent optional (default true)
   - messages
-  - messages_file
+    - durable optional      (true or false) (default alse)
+    - contentType optional  
+    - contentEncoding optional
+    - persistent optional (default true)
+    - headers optional
+      - name: value
 
 ```
 
-Example:
+## Examples:
 
+### Publisher (workQ)
 ```yaml
+name: TestSuite RabbitMQ
+vars:
+  addrs: 'amqp://localhost:5672'
+  user: 
+  password: 
+testcases:
+  - name: RabbitMQ publish (work Q)
+    steps:
+      - type: rabbitmq
+        addrs: "{{.addrs}}"
+        user: "{{.user}}"
+        password: "{{.password}}"
+        clientType: publisher
+        qName: TEST
+        messages: 
+          - value: '{"a": "b"}'
+            contentType: application/json
+            contentEncoding: utf8
+            persistant: false
+            headers: 
+              myCustomHeader: value
+              myCustomHeader2: value2
+```
 
+### Subscriber (workQ)
+```yaml
+name: TestSuite RabbitMQ
+vars:
+  addrs: 'amqp://localhost:5672'
+  user: 
+  password: 
+  - name: RabbitMQ subscribe testcase
+    steps:
+      - type: rabbitmq
+        addrs: "{{.addrs}}"
+        user: "{{.user}}"
+        password: "{{.password}}"
+        clientType: subscriber
+        qName: "{{.qName}}"
+        durable: true
+        messageLimit: 1
+        assertions: 
+          - result.bodyjson.bodyjson0.a ShouldEqual b   
+          - result.headers.headers0.mycustomheader ShouldEqual value
+          - result.headers.headers0.mycustomheader2 ShouldEqual value2
+          - result.messages.messages0.contentencoding ShouldEqual utf8
+          - result.messages.messages0.contenttype ShouldEqual application/json
+```
+
+### Publisher (pubsub)
+```yaml
+name: TestSuite RabbitMQ
+vars:
+  addrs: 'amqp://localhost:5672'
+  user: 
+  password: 
+testcases:
+  - name: RabbitMQ publish (work Q)
+    steps:
+      - type: rabbitmq
+        addrs: "{{.addrs}}"
+        user: "{{.user}}"
+        password: "{{.password}}"
+        clientType: publisher
+        exchange: exchange_test
+        routingKey: pubsub_test
+        messages: 
+          - value: '{"a": "b"}'
+            contentType: application/json
+            contentEncoding: utf8
+            persistant: false
+            headers: 
+              myCustomHeader: value
+              myCustomHeader2: value2
+```
+
+### Subscriber (pubsub)
+```yaml
+name: TestSuite RabbitMQ
+vars:
+  addrs: 'amqp://localhost:5672'
+  user: 
+  password: 
+  - name: RabbitMQ subscribe testcase
+    steps:
+      - type: rabbitmq
+        addrs: "{{.addrs}}"
+        user: "{{.user}}"
+        password: "{{.password}}"
+        clientType: subscriber
+        exchange: exchange_test
+        routingKey: pubsub_test
+        messageLimit: 1
+        assertions: 
+          - result.bodyjson.bodyjson0.a ShouldEqual b   
+          - result.headers.headers0.mycustomheader ShouldEqual value
+          - result.headers.headers0.mycustomheader2 ShouldEqual value2
+          - result.messages.messages0.contentencoding ShouldEqual utf8
+          - result.messages.messages0.contenttype ShouldEqual application/json
 ```
--- executors/rabbitmq/executor.go
+++ executors/rabbitmq/executor.go
@@ -22,7 +22,11 @@ func New() venom.Executor {
 
 //Message represents the object sended or received from rabbitmq
 type Message struct {
-	Value string `json:"value" yaml:"value"`
+	Value           string     `json:"value" yaml:"value"`
+	Headers         amqp.Table `json:"headers" yaml:"headers"`
+	Persistent      bool       `json:"persistent" yaml:"persistent"`
+	ContentType     string     `json:"content_type" yaml:"contentType"`
+	ContentEncoding string     `json:"content_encoding" yaml:"contentEncoding"`
 }
 
 // Executor represents a Test Exec
@@ -50,14 +54,9 @@ type Executor struct {
 	//Represents the limit of message will be read. After limit, consumer stop read message
 	MessageLimit int `json:"message_limit" yaml:"messageLimit"`
 
-	Persistent   bool   `json:"persistent" yaml:"persistent"`
-	DeliveryMode uint8  `json:"delivery_mode" yaml:"deliveryMode"`
-	ContentType  string `json:"content_type" yaml:"contentType"`
 	//Used when ClientType is producer
 	//Messages represents the message sended by producer
 	Messages []Message `json:"messages" yaml:"messages"`
-	//MessagesFile represents the messages into the file sended by producer (messages field would be ignored)
-	MessagesFile string `json:"messages_file" yaml:"messages_file"`
 }
 
 // Result represents a step result.
@@ -68,6 +67,7 @@ type Result struct {
 	Body        []string      `json:"body" yaml:"body"`
 	Messages    []interface{} `json:"messages" yaml:"messages"`
 	BodyJSON    []interface{} `json:"bodyJSON" yaml:"bodyJSON"`
+	Headers     []amqp.Table  `json:"headers" yaml:"headers"`
 	Err         string        `json:"error" yaml:"error"`
 }
 
@@ -93,10 +93,6 @@ func (Executor) Run(testCaseContext venom.TestCaseContext, l venom.Logger, step 
 
 	result := Result{Executor: e}
 
-	if len(e.QName) == 0 {
-		return nil, fmt.Errorf("QName is mandatory")
-	}
-
 	// Default values
 	if len(e.Addrs) == 0 {
 		e.Addrs = "amqp:/localhost:5672"
@@ -110,13 +106,6 @@ func (Executor) Run(testCaseContext venom.TestCaseContext, l venom.Logger, step 
 	if len(e.ExchangeType) == 0 {
 		e.ExchangeType = amqp.ExchangeFanout
 	}
-
-	if e.DeliveryMode == 0 {
-		e.DeliveryMode = amqp.Persistent
-		if e.Persistent == false {
-			e.DeliveryMode = amqp.Transient
-		}
-	}
 	if e.MessageLimit == 0 {
 		e.MessageLimit = 1
 	}
@@ -129,7 +118,7 @@ func (Executor) Run(testCaseContext venom.TestCaseContext, l venom.Logger, step 
 		}
 	} else if e.ClientType == "subscriber" {
 		var err error
-		result.Body, result.BodyJSON, result.Messages, err = e.consumeMessages(l)
+		result.Body, result.BodyJSON, result.Messages, result.Headers, err = e.consumeMessages(l)
 		if err != nil {
 			result.Err = err.Error()
 			return nil, err
@@ -185,6 +174,9 @@ func (e Executor) publishMessages(workdir string, l venom.Logger) error {
 		}
 		l.Debugf("exchange declared '%s' '%s'", e.Exchange, e.ExchangeType)
 	} else {
+		if len(e.QName) == 0 {
+			return fmt.Errorf("QName is mandatory")
+		}
 		routingKey = e.QName
 		_, err := ch.QueueDeclare(
 			e.QName,   // name
@@ -200,23 +192,25 @@ func (e Executor) publishMessages(workdir string, l venom.Logger) error {
 		l.Debugf("Q declated '%s'", e.QName)
 	}
 
-	// Read from file
-	if len(e.MessagesFile) > 0 {
-
-	}
-
 	l.Debugf("Message to send: %d", len(e.Messages))
 	for i := range e.Messages {
+		deliveryMode := amqp.Persistent
+		if e.Messages[i].Persistent == false {
+			deliveryMode = amqp.Transient
+		}
 		err = ch.Publish(
 			e.Exchange, // exchange
 			routingKey, // routing key
 			false,      // mandatory
 			false,      // imediate
 			amqp.Publishing{
-				DeliveryMode: e.DeliveryMode,
-				ContentType:  e.ContentType,
-				Body:         []byte(e.Messages[i].Value),
+				DeliveryMode:    deliveryMode,
+				ContentType:     e.Messages[i].ContentType,
+				ContentEncoding: e.Messages[i].ContentEncoding,
+				Body:            []byte(e.Messages[i].Value),
+				Headers:         e.Messages[i].Headers,
 			})
+
 		if err != nil {
 			return err
 		}
@@ -226,28 +220,41 @@ func (e Executor) publishMessages(workdir string, l venom.Logger) error {
 	return nil
 }
 
-func (e Executor) consumeMessages(l venom.Logger) ([]string, []interface{}, []interface{}, error) {
+func (e Executor) consumeMessages(l venom.Logger) ([]string, []interface{}, []interface{}, []amqp.Table, error) {
 	l.Debugf("consumeMessages")
 	uri, err := amqp.ParseURI(e.Addrs)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 	uri.Username = e.User
 	uri.Password = e.Password
 
 	conn, err := amqp.Dial(uri.String())
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 	l.Debugf("connection opened")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 	l.Debugf("channel opened")
 	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		e.QName,   // name
+		e.Durable, // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	l.Debugf("Q declated '%s'", q.Name)
 
 	// If an exchange if defined
 	if len(e.Exchange) > 0 {
@@ -261,22 +268,9 @@ func (e Executor) consumeMessages(l venom.Logger) ([]string, []interface{}, []in
 			nil,            // arguments
 		)
 		if err != nil {
-			return nil, nil, nil, err
+			return nil, nil, nil, nil, err
 		}
 		l.Debugf("exchange declared '%s' '%s'", e.Exchange, e.ExchangeType)
-
-		q, err := ch.QueueDeclare(
-			"",        // name
-			e.Durable, // durable
-			false,     // delete when unused
-			false,     // exclusive
-			false,     // no-wait
-			nil,       // arguments
-		)
-		if err != nil {
-			return nil, nil, nil, err
-		}
-		l.Debugf("Q declated '%s'", e.QName)
 
 		err = ch.QueueBind(
 			q.Name,       // queue name
@@ -286,35 +280,26 @@ func (e Executor) consumeMessages(l venom.Logger) ([]string, []interface{}, []in
 			nil,          // arguments
 		)
 		if err != nil {
-			return nil, nil, nil, err
+			return nil, nil, nil, nil, err
 		}
-		l.Debugf("Q binded '%s' '%s'", e.QName, e.RoutingKey)
-	} else {
-		_, err := ch.QueueDeclare(
-			e.QName,   // name
-			e.Durable, // durable
-			false,     // delete when unused
-			false,     // exclusive
-			false,     // no-wait
-			nil,       // arguments
-		)
-		if err != nil {
-			return nil, nil, nil, err
-		}
-		l.Debugf("Q declated '%s'", e.QName)
+		l.Debugf("Q binded '%s' '%s'", q.Name, e.RoutingKey)
 	}
 
 	body := []string{}
 	bodyJSON := []interface{}{}
 	messages := []interface{}{}
+	headers := []amqp.Table{}
 
 	for i := 0; i < e.MessageLimit; i++ {
 		l.Debugf("Read message n° %d", i)
 
-		msg, ok, err := ch.Get(e.QName, true) // Read one message from RabbitMQ
+		msg, ok, err := ch.Get(q.Name, true) // Read one message from RabbitMQ
 		if err != nil {
-			return nil, nil, nil, err
+			return nil, nil, nil, nil, err
 		}
+
+		headers = append(headers, msg.Headers)
+		messages = append(messages, msg)
 
 		l.Debugf("message: %t %s %s %s", ok, msg.RoutingKey, msg.MessageId, msg.ContentType)
 
@@ -335,5 +320,5 @@ func (e Executor) consumeMessages(l venom.Logger) ([]string, []interface{}, []in
 
 	}
 
-	return body, bodyJSON, messages, err
+	return body, bodyJSON, messages, headers, err
 }
