384107157082542bde5ce801fe0a3b285ebf6de7
Kevin RAMAGE <kevinramage@hotmail.fr>
2020-03-07 18:34:19 +0100

    Add new feature to upload file

--- executors/web/types.go
+++ executors/web/types.go
@@ -7,6 +7,7 @@ type Action struct {
 	Find     string    `yaml:"find,omitempty"`
 	Navigate *Navigate `yaml:"navigate,omitempty"`
 	Wait     int64     `yaml:"wait,omitempty"`
+	UploadFile *UploadFile `yaml:"uploadFile,omitempty"`
 }
 
 // Fill represents informations needed to fill input/textarea
@@ -16,12 +17,21 @@ type Fill struct {
 	Key  *string `yaml:"key,omitempty"`
 }
 
+// Click represents informations needed to click on web components
 type Click struct {
 	Find string `yaml:"find,omitempty"`
 	Wait int64  `yaml:"wait"`
 }
 
+// Navigate represents informations needed to navigate on defined url
 type Navigate struct {
 	Url   string `yaml:"url,omitempty"`
 	Reset bool   `yaml:"reset,omitempty"`
+}
+
+// UploadFile represents informations needed to upload files
+type UploadFile struct {
+	Find string `yaml:"find,omitempty"`
+	Files []string `yaml:"files,omitempty"`
+	Wait int64 `yaml:"wait,omitempty"`
 }
