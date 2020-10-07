5aff24a1b8b5523fa7cb36c79c35a257da551b2d
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-10-07 18:08:02 +0200

    refactor(executor/web): isolate dump html file
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- executors/web/web.go
+++ executors/web/web.go
@@ -64,11 +64,56 @@ func (Executor) Run(testCaseContext venom.TestCaseContext, l venom.Logger, step 
 	if err := mapstructure.Decode(step, &e); err != nil {
 		return nil, err
 	}
-	r := &Result{Executor: e}
 
-	// Check action to realise
+	result, err := e.runAction(l, ctx.Page)
+	if err != nil {
+		if errg := generateErrorHTMLFile(l, ctx.Page, ctx.Name); errg != nil {
+			l.Warnf("Error while generate HTML file: %v", errg)
+			return nil, err
+		}
+		return nil, err
+	}
+
+	// take a screenshot
+	if e.Screenshot != "" {
+		if err := ctx.Page.Screenshot(e.Screenshot); err != nil {
+			return nil, err
+		}
+		if err := generateErrorHTMLFile(l, ctx.Page, ctx.Name); err != nil {
+			l.Warnf("Error while generate HTML file: %v", err)
+			return nil, err
+		}
+	}
+
+	// Get page title (Check the absence of popup before the page title collect to avoid error)
+	if _, err := ctx.Page.PopupText(); err != nil {
+		title, err := ctx.Page.Title()
+		if err != nil {
+			return nil, err
+		}
+		result.Title = title
+	}
+
+	// Get page url (Check the absence of popup before the page url collect to avoid error)
+	if _, err := ctx.Page.PopupText(); err != nil {
+		url, errU := ctx.Page.URL()
+		if errU != nil {
+			return nil, fmt.Errorf("Cannot get URL: %s", errU)
+		}
+		result.URL = url
+	}
+
+	elapsed := time.Since(start)
+	result.TimeSeconds = elapsed.Seconds()
+	result.TimeHuman = fmt.Sprintf("%s", elapsed)
+
+	return executors.Dump(result)
+}
+
+func (e Executor) runAction(l venom.Logger, page *agouti.Page) (*Result, error) {
+	r := &Result{Executor: e}
 	if e.Action.Click != nil {
-		s, err := find(testCaseContext, l, ctx.Page, e.Action.Click.Find, r)
+		s, err := find(page, e.Action.Click.Find, r)
 		if err != nil {
 			return nil, err
 		}
@@ -80,7 +125,7 @@ func (Executor) Run(testCaseContext venom.TestCaseContext, l venom.Logger, step 
 		}
 	} else if e.Action.Fill != nil {
 		for _, f := range e.Action.Fill {
-			s, err := findOne(testCaseContext, l, ctx.Page, f.Find, r)
+			s, err := findOne(page, f.Find, r)
 			if err != nil {
 				return nil, err
 			}
@@ -94,7 +139,7 @@ func (Executor) Run(testCaseContext venom.TestCaseContext, l venom.Logger, step 
 			}
 		}
 	} else if e.Action.Find != "" {
-		s, err := find(testCaseContext, l, ctx.Page, e.Action.Find, r)
+		s, err := find(page, e.Action.Find, r)
 		if err != nil {
 			return nil, err
 		} else if s != nil {
@@ -110,33 +155,29 @@ func (Executor) Run(testCaseContext venom.TestCaseContext, l venom.Logger, step 
 			}
 		}
 	} else if e.Action.Navigate != nil {
-		if err := ctx.Page.Navigate(e.Action.Navigate.Url); err != nil {
+		if err := page.Navigate(e.Action.Navigate.Url); err != nil {
 			return nil, err
 		}
 		if e.Action.Navigate.Reset {
-			if err := ctx.Page.Reset(); err != nil {
+			if err := page.Reset(); err != nil {
 				return nil, err
 			}
-			if err := ctx.Page.Navigate(e.Action.Navigate.Url); err != nil {
+			if err := page.Navigate(e.Action.Navigate.Url); err != nil {
 				return nil, err
 			}
 		}
 	} else if e.Action.Wait != 0 {
 		time.Sleep(time.Duration(e.Action.Wait) * time.Second)
 	} else if e.Action.ConfirmPopup {
-		if ctx.TestCase.Context["driver"] != "phantomjs" {
-			if err := ctx.Page.ConfirmPopup(); err != nil {
-				return nil, err
-			}
+		if err := page.ConfirmPopup(); err != nil {
+			return nil, err
 		}
 	} else if e.Action.CancelPopup {
-		if ctx.TestCase.Context["driver"] != "phantomjs" {
-			if err := ctx.Page.CancelPopup(); err != nil {
-				return nil, err
-			}
+		if err := page.CancelPopup(); err != nil {
+			return nil, err
 		}
 	} else if e.Action.Select != nil {
-		s, err := findOne(testCaseContext, l, ctx.Page, e.Action.Select.Find, r)
+		s, err := findOne(page, e.Action.Select.Find, r)
 		if err != nil {
 			return nil, err
 		}
@@ -147,7 +188,7 @@ func (Executor) Run(testCaseContext venom.TestCaseContext, l venom.Logger, step 
 			time.Sleep(time.Duration(e.Action.Select.Wait) * time.Second)
 		}
 	} else if e.Action.UploadFile != nil {
-		s, err := find(testCaseContext, l, ctx.Page, e.Action.UploadFile.Find, r)
+		s, err := find(page, e.Action.UploadFile.Find, r)
 		if err != nil {
 			return nil, err
 		}
@@ -160,85 +201,53 @@ func (Executor) Run(testCaseContext venom.TestCaseContext, l venom.Logger, step 
 			time.Sleep(time.Duration(e.Action.UploadFile.Wait) * time.Second)
 		}
 	} else if e.Action.SelectFrame != nil {
-		s, err := findOne(testCaseContext, l, ctx.Page, e.Action.SelectFrame.Find, r)
+		s, err := findOne(page, e.Action.SelectFrame.Find, r)
 		if err != nil {
 			return nil, err
 		}
 		if elements, errElements := s.Elements(); errElements == nil {
-			if errSelectFrame := ctx.Page.Session().Frame(elements[0]); errSelectFrame != nil {
+			if errSelectFrame := page.Session().Frame(elements[0]); errSelectFrame != nil {
 				return nil, errSelectFrame
 			}
 		} else {
 			return nil, errElements
 		}
 	} else if e.Action.SelectRootFrame {
-		if err := ctx.Page.SwitchToRootFrame(); err != nil {
+		if err := page.SwitchToRootFrame(); err != nil {
 			return nil, err
 		}
 	} else if e.Action.NextWindow {
-		if err := ctx.Page.NextWindow(); err != nil {
+		if err := page.NextWindow(); err != nil {
 			return nil, err
 		}
 	} else if e.Action.HistoryAction != "" {
 		switch strings.ToLower(e.Action.HistoryAction) {
 		case "back":
-			if err := ctx.Page.Back(); err != nil {
+			if err := page.Back(); err != nil {
 				return nil, err
 			}
 		case "refresh":
-			if err := ctx.Page.Refresh(); err != nil {
+			if err := page.Refresh(); err != nil {
 				return nil, err
 			}
 		case "forward":
-			if err := ctx.Page.Forward(); err != nil {
+			if err := page.Forward(); err != nil {
 				return nil, err
 			}
 		default:
 			return nil, fmt.Errorf("History action '%s' is invalid", e.Action.HistoryAction)
 		}
 	}
-
-	// take a screenshot
-	if e.Screenshot != "" {
-		if err := ctx.Page.Screenshot(e.Screenshot); err != nil {
-			return nil, err
-		}
-	}
-
-	// Get page title (Check the absence of popup before the page title collect to avoid error)
-	if _, err := ctx.Page.PopupText(); err != nil {
-		title, err := ctx.Page.Title()
-		if err != nil {
-			return nil, err
-		}
-		r.Title = title
-	}
-
-	// Get page url (Check the absence of popup before the page url collect to avoid error)
-	if _, err := ctx.Page.PopupText(); err != nil {
-		url, errU := ctx.Page.URL()
-		if errU != nil {
-			return nil, fmt.Errorf("Cannot get URL: %s", errU)
-		}
-		r.URL = url
-	}
-
-	elapsed := time.Since(start)
-	r.TimeSeconds = elapsed.Seconds()
-	r.TimeHuman = fmt.Sprintf("%s", elapsed)
-
-	return executors.Dump(r)
+	return r, nil
 }
 
-func find(testCaseContext venom.TestCaseContext, logger venom.Logger, page *agouti.Page, search string, r *Result) (*agouti.Selection, error) {
+func find(page *agouti.Page, search string, r *Result) (*agouti.Selection, error) {
 	s := page.Find(search)
 	if s == nil {
-		generateHtmlFile(testCaseContext, logger, page)
 		return nil, fmt.Errorf("Cannot find element %s", search)
 	}
 	nbElement, errC := s.Count()
 	if errC != nil {
-		generateHtmlFile(testCaseContext, logger, page)
 		if !strings.Contains(errC.Error(), "element not found") {
 			return nil, fmt.Errorf("Cannot count element %s: %s", search, errC)
 		}
@@ -248,34 +257,28 @@ func find(testCaseContext venom.TestCaseContext, logger venom.Logger, page *agou
 	return s, nil
 }
 
-func findOne(testCaseContext venom.TestCaseContext, logger venom.Logger, page *agouti.Page, search string, r *Result) (*agouti.Selection, error) {
+func findOne(page *agouti.Page, search string, r *Result) (*agouti.Selection, error) {
 	s := page.Find(search)
 	if s == nil {
-		generateHtmlFile(testCaseContext, logger, page)
 		return nil, fmt.Errorf("Cannot find element %s", search)
 	}
 	nbElement, errC := s.Count()
 	if errC != nil {
-		generateHtmlFile(testCaseContext, logger, page)
 		return nil, fmt.Errorf("Cannot find element %s: %s", search, errC)
 	}
 	if nbElement != 1 {
-		generateHtmlFile(testCaseContext, logger, page)
 		return nil, fmt.Errorf("Find %d elements", nbElement)
 	}
 	return s, nil
 }
 
-// Generate an HTML file in error case to identify clearly the error
-func generateHtmlFile(context venom.TestCaseContext, logger venom.Logger, page *agouti.Page) {
+// generateErrorHTMLFile generates an HTML file in error case to identify clearly the error
+func generateErrorHTMLFile(logger venom.Logger, page *agouti.Page, name string) error {
 	html, err := page.HTML()
-	if err == nil {
-		fileName := context.GetName() + ".html"
-		logger.Infof("Error during an element selection, the content of the HTML page is saved in %s\n", fileName)
-		if errSave := ioutil.WriteFile(fileName, []byte(html), 0644); errSave != nil {
-			logger.Errorf("Cannot save html page in %s: %s\n", fileName, errSave)
-		}
-	} else {
-		logger.Errorf("Cannot get html page: %s\n", err)
+	if err != nil {
+		return err
 	}
+	filename := name + ".dump.html"
+	logger.Infof("Content of the HTML page is saved in %s", filename)
+	return ioutil.WriteFile(filename, []byte(html), 0644)
 }
