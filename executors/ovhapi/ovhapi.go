1bd7c80c7ffc4325cf6f3da830cbf568fba9b0f9
Romain Beuque <556072+rbeuque74@users.noreply.github.com>
2021-02-24 09:43:25 +0100

    feat(executor/ovhapi): add proxy and resolve to ovhapi executor (#365)
    Signed-off-by: Romain Beuque <romain.beuque@ovhcloud.com>

--- executors/ovhapi/ovhapi.go
+++ executors/ovhapi/ovhapi.go
@@ -3,9 +3,14 @@ package ovhapi
 import (
 	"context"
 	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -30,12 +35,20 @@ type Headers map[string]string
 
 // Executor struct. Json and yaml descriptor are used for json output
 type Executor struct {
-	Method   string  `json:"method" yaml:"method"`
-	NoAuth   bool    `json:"no_auth" yaml:"noAuth"`
-	Path     string  `json:"path" yaml:"path"`
-	Body     string  `json:"body" yaml:"body"`
-	BodyFile string  `json:"bodyfile" yaml:"bodyfile"`
-	Headers  Headers `json:"headers" yaml:"headers"`
+	Endpoint          string   `json:"endpoint" yaml:"endpoint"`
+	ApplicationKey    string   `json:"applicationKey" yaml:"applicationKey"`
+	ApplicationSecret string   `json:"applicationSecret" yaml:"applicationSecret"`
+	ConsumerKey       string   `json:"consumerKey" yaml:"consumerKey"`
+	NoAuth            *bool    `json:"noAuth" yaml:"noAuth"`
+	Headers           Headers  `json:"headers" yaml:"headers"`
+	Resolve           []string `json:"resolve" yaml:"resolve"`
+	Proxy             string   `json:"proxy" yaml:"proxy"`
+	TLSRootCA         string   `json:"tlsRootCA" yaml:"tlsRootCA"`
+
+	Method   string `json:"method" yaml:"method"`
+	Path     string `json:"path" yaml:"path"`
+	Body     string `json:"body" yaml:"body"`
+	BodyFile string `json:"bodyFile" yaml:"bodyFile"`
 }
 
 // Result represents a step result. Json and yaml descriptor are used for json output
@@ -67,11 +80,33 @@ func (Executor) Run(ctx context.Context, step venom.TestStep) (interface{}, erro
 	}
 
 	// Get context
-	var endpoint = venom.StringVarFromCtx(ctx, "ovh.endpoint")
-	var applicationKey = venom.StringVarFromCtx(ctx, "ovh.applicationKey")
-	var applicationSecret = venom.StringVarFromCtx(ctx, "ovh.applicationSecret")
-	var consumerKey = venom.StringVarFromCtx(ctx, "ovh.consumerKey")
-	var insecure = venom.BoolVarFromCtx(ctx, "ovh.insecureTLS")
+	if e.Endpoint == "" {
+		e.Endpoint = venom.StringVarFromCtx(ctx, "ovh.endpoint")
+	}
+	if e.ApplicationKey == "" {
+		e.ApplicationKey = venom.StringVarFromCtx(ctx, "ovh.applicationKey")
+	}
+	if e.ApplicationSecret == "" {
+		e.ApplicationSecret = venom.StringVarFromCtx(ctx, "ovh.applicationSecret")
+	}
+	if e.ConsumerKey == "" {
+		e.ConsumerKey = venom.StringVarFromCtx(ctx, "ovh.consumerKey")
+	}
+	if e.NoAuth == nil {
+		noauth := venom.BoolVarFromCtx(ctx, "ovh.noAuth")
+		e.NoAuth = &noauth
+	}
+	noAuth := *(e.NoAuth)
+	if len(e.Resolve) == 0 {
+		e.Resolve = venom.StringSliceVarFromCtx(ctx, "ovh.resolve")
+	}
+	if e.Proxy == "" {
+		e.Proxy = venom.StringVarFromCtx(ctx, "ovh.proxy")
+	}
+	if e.TLSRootCA == "" {
+		e.TLSRootCA = venom.StringVarFromCtx(ctx, "ovh.tlsRootCA")
+	}
+
 	var headers = venom.StringMapStringVarFromCtx(ctx, "ovh.headers")
 	var workdir = venom.StringVarFromCtx(ctx, "venom.testsuite.workdir")
 
@@ -86,19 +121,79 @@ func (Executor) Run(ctx context.Context, step venom.TestStep) (interface{}, erro
 	start := time.Now()
 	// prepare ovh api client
 	client, err := ovh.NewClient(
-		endpoint,
-		applicationKey,
-		applicationSecret,
-		consumerKey,
+		e.Endpoint,
+		e.ApplicationKey,
+		e.ApplicationSecret,
+		e.ConsumerKey,
 	)
 	if err != nil {
 		return nil, err
 	}
 
-	if insecure {
-		client.Client.Transport = &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	var tr *http.Transport
+
+	if e.TLSRootCA != "" {
+		if tr == nil {
+			tr = http.DefaultTransport.(*http.Transport).Clone()
 		}
+
+		if tr.TLSClientConfig == nil {
+			tr.TLSClientConfig = &tls.Config{}
+		}
+		caCertPool, err := x509.SystemCertPool()
+		if err != nil {
+			venom.Warning(ctx, "http: tls: failed to load default system cert pool, fallback to an empty cert pool")
+			caCertPool = x509.NewCertPool()
+		}
+
+		if ok := caCertPool.AppendCertsFromPEM([]byte(e.TLSRootCA)); !ok {
+			return nil, errors.New("TLSRootCA: failed to add a certificate to the cert pool")
+		}
+
+		tr.TLSClientConfig.RootCAs = caCertPool
+	}
+
+	if len(e.Resolve) > 0 {
+		if tr == nil {
+			tr = http.DefaultTransport.(*http.Transport).Clone()
+		}
+
+		tr.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+			// resolve can contains foo.com:443:127.0.0.1
+			for _, r := range e.Resolve {
+				tuple := strings.Split(r, ":")
+				if len(tuple) != 3 {
+					return nil, fmt.Errorf("invalid value for resolve attribute: %v", e.Resolve)
+				}
+				if addr == tuple[0]+":"+tuple[1] {
+					addr = tuple[2] + ":" + tuple[1]
+				}
+			}
+
+			dialer := &net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+				DualStack: true,
+			}
+			return dialer.DialContext(ctx, network, addr)
+		}
+	}
+
+	if e.Proxy != "" {
+		proxyURL, err := url.Parse(e.Proxy)
+		if err != nil {
+			return nil, err
+		}
+
+		if tr == nil {
+			tr = http.DefaultTransport.(*http.Transport).Clone()
+		}
+
+		tr.Proxy = http.ProxyURL(proxyURL)
+	}
+
+	if tr != nil {
+		client.Client.Transport = tr
 	}
 
 	// get request body from file or from field
@@ -107,7 +202,7 @@ func (Executor) Run(ctx context.Context, step venom.TestStep) (interface{}, erro
 		return nil, err
 	}
 
-	req, err := client.NewRequest(e.Method, e.Path, requestBody, !e.NoAuth)
+	req, err := client.NewRequest(e.Method, e.Path, requestBody, !noAuth)
 	if err != nil {
 		return nil, err
 	}
@@ -120,6 +215,10 @@ func (Executor) Run(ctx context.Context, step venom.TestStep) (interface{}, erro
 		for key := range e.Headers {
 			req.Header.Set(key, e.Headers[key])
 		}
+	}
+
+	if h := req.Header.Get("Host"); h != "" {
+		req.Host = h
 	}
 
 	// do api call
