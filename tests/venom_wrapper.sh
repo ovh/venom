d3fb381ee638c8256dd1fffabd30ccdab202541f
richardlt <richard.le.terrier@gmail.com>
2021-03-22 19:43:26 +0100

    wip compute coverage
    Signed-off-by: richardlt <richard.le.terrier@gmail.com>

--- tests/venom_wrapper.sh
+++ tests/venom_wrapper.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+DIR=`dirname $0`
 NANO_TIMESTAMP=$(date +%s%N)
 
 # Prepare args file from current args
-rm -f ./$NANO_TIMESTAMP.args.file
+rm -f $DIR/$NANO_TIMESTAMP.args.file
 for v in "$@"
 do
-  echo $v >> ./$NANO_TIMESTAMP.args.file
+  echo $v >> $DIR/$NANO_TIMESTAMP.args.file
 done
 
 # Exec venom test binary, capture logs and exit code
-./venom.test -test.coverprofile=./$NANO_TIMESTAMP.coverprofile -test.run="^TestBincoverRunMain$" -args-file=./$NANO_TIMESTAMP.args.file 1> $NANO_TIMESTAMP.out 2> $NANO_TIMESTAMP.error.out
+$DIR/venom.test -test.coverprofile=$DIR/$NANO_TIMESTAMP.coverprofile -test.run="^TestBincoverRunMain$" -args-file=$DIR/$NANO_TIMESTAMP.args.file 1> $DIR/$NANO_TIMESTAMP.out 2> $DIR/$NANO_TIMESTAMP.error.out
 EXI=$?
 
 # Print venom log on stdout and go test log in a dedicated file
@@ -21,17 +22,17 @@ do
     P=`printf '%s' "$p" | sed -e "s/START_BINCOVER_METADATA$//"`
     printf '%s\n' "$P"
     END_CTL_OUT=true
-    echo "START_BINCOVER_METADATA" >> $NANO_TIMESTAMP.test.out
+    echo "START_BINCOVER_METADATA" >> $DIR/$NANO_TIMESTAMP.test.out
   else
     if [ "$p" == "START_BINCOVER_METADATA" ]; then
       END_CTL_OUT=true
     fi
     if $END_CTL_OUT; then
-      echo "$p" >> $NANO_TIMESTAMP.test.out
+      echo "$p" >> $DIR/$NANO_TIMESTAMP.test.out
     else
       printf '%s\n' "$p"
     fi
   fi
-done < ./$NANO_TIMESTAMP.out
+done < $DIR/$NANO_TIMESTAMP.out
 
 exit $EXI
