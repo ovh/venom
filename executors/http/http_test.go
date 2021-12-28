bc3d1ee42be76dd30f6c8aac510e8f24c65503fa
francois  samin <francois.samin@corp.ovh.com>
2021-12-28 14:47:41 +0100

    test: generate TLS key and crt in the fly
    Signed-off-by: francois  samin <francois.samin@corp.ovh.com>

--- executors/http/http_test.go
+++ executors/http/http_test.go
@@ -3,16 +3,41 @@ package http
 import (
 	"context"
 	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+func generateClientFile(t *testing.T) (string, string) {
+	TLSClientKey, err := os.CreateTemp(os.TempDir(), "TLSClientKey.*.key")
+	require.NoError(t, err)
+	TLSClientKeyFileName := TLSClientKey.Name()
+	t.Logf("generating file %q", TLSClientKeyFileName)
+	cmd := exec.Command("openssl", "genrsa", "-out", TLSClientKeyFileName, "2048")
+	output, err := cmd.CombinedOutput()
+	t.Log(string(output))
+	require.NoError(t, err)
+
+	TLSClientCert, err := os.CreateTemp(os.TempDir(), "TLSClientCert.*.crt")
+	require.NoError(t, err)
+	TLSClientCertFilename := TLSClientCert.Name()
+	t.Logf("generating file %q", TLSClientCertFilename)
+	cmd = exec.Command("openssl", "req", "-batch", "-subj", "/C=GB/ST=Yorks/L=York/O=MyCompany Ltd./OU=IT/CN=mysubdomain.mydomain.com", "-new", "-x509", "-sha256", "-key", TLSClientKeyFileName, "-out", TLSClientCertFilename, "-days", "365")
+	output, err = cmd.CombinedOutput()
+	t.Log(string(output))
+	require.NoError(t, err)
+
+	return TLSClientKeyFileName, TLSClientCertFilename
+}
+
 func TestExecutor_TLSOptions_From_File(t *testing.T) {
+	TLSClientKeyFileName, TLSClientCertFilename := generateClientFile(t)
+
 	e := Executor{
 		IgnoreVerifySSL: true,
-		TLSClientCert:   "../../tests/http/tls/http-client-tls.crt",
-		TLSClientKey:    "../../tests/http/tls/http-client-tls.key",
+		TLSClientCert:   TLSClientCertFilename,
+		TLSClientKey:    TLSClientKeyFileName,
 		TLSRootCA:       "../../tests/http/tls/digicert-root-ca.crt",
 	}
 	opts, err := e.TLSOptions(context.Background())
@@ -21,9 +46,11 @@ func TestExecutor_TLSOptions_From_File(t *testing.T) {
 }
 
 func TestExecutor_TLSOptions_From_String(t *testing.T) {
-	TLSClientCert, err := os.ReadFile("../../tests/http/tls/http-client-tls.crt")
+	TLSClientKeyFileName, TLSClientCertFilename := generateClientFile(t)
+
+	TLSClientCert, err := os.ReadFile(TLSClientCertFilename)
 	require.NoError(t, err)
-	TLSClientKey, err := os.ReadFile("../../tests/http/tls/http-client-tls.key")
+	TLSClientKey, err := os.ReadFile(TLSClientKeyFileName)
 	require.NoError(t, err)
 	TLSRootCA, err := os.ReadFile("../../tests/http/tls/digicert-root-ca.crt")
 	require.NoError(t, err)
--- tests/http/tls/README.md
+++
@@ -1,4 +0,0 @@
-# IMPORTANT NOTICE
-
-The crt and key file are for testing purpose only.
-**DO NOT USE**
