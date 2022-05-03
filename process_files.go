475bf7f37a65eeee615ac96579296d6ffa39fe56
francois  samin <francois.samin@corp.ovh.com>
2022-05-03 11:39:57 +0200

    wip
    Signed-off-by: francois  samin <francois.samin@corp.ovh.com>

--- process_files.go
+++ process_files.go
@@ -81,8 +81,10 @@ func (v *Venom) readFiles(ctx context.Context, filesPath []string) (err error) {
 			return errors.Wrapf(err, "unable to get vars from file %q", f)
 		}
 
+		fmt.Println(fromPartial)
+
 		var varsFromPartial map[string]string
-		if len(varsFromPartial) > 0 {
+		if len(fromPartial) > 0 {
 			varsFromPartial, err = DumpStringPreserveCase(fromPartial)
 			if err != nil {
 				return errors.Wrapf(err, "unable to parse variables")
