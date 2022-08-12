f0562157c7ba57b0ef6830e06489981218b009be
Sambhav Kothari <sambhavs.email@gmail.com>
2022-08-12 18:35:40 +0100

    Add a registry variable to source executors from
    Signed-off-by: Sambhav Kothari <sambhavs.email@gmail.com>

--- cmd/venom/main.go
+++ cmd/venom/main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
+	"github.com/ovh/venom/cmd/venom/root"
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
-
-	"github.com/ovh/venom/cmd/venom/run"
-	"github.com/ovh/venom/cmd/venom/update"
-	"github.com/ovh/venom/cmd/venom/version"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "venom",
-	Short: "Venom aim to create, manage and run your integration tests with efficiency",
-}
-
 func main() {
-	addCommands()
-
-	if err := rootCmd.Execute(); err != nil {
+	if err := root.New().Execute(); err != nil {
 		log.Fatalf("Err:%s", err)
 	}
-}
-
-//AddCommands adds child commands to the root command rootCmd.
-func addCommands() {
-	rootCmd.AddCommand(run.Cmd)
-	rootCmd.AddCommand(version.Cmd)
-	rootCmd.AddCommand(update.Cmd)
 }
