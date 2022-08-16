aaa2acb2b27cb49b6e6aa626ef2df92e2d836e8d
Sambhav Kothari <sambhavs.email@gmail.com>
2022-08-16 09:04:59 +0100

    Add a registry variable to source executors from (#571)
    Signed-off-by: Sambhav Kothari <sambhavs.email@gmail.com>

--- cmd/venom/run/cmd.go
+++ cmd/venom/run/cmd.go
@@ -19,22 +19,7 @@ import (
 
 	"github.com/ovh/cds/sdk/interpolate"
 	"github.com/ovh/venom"
-	"github.com/ovh/venom/executors/amqp"
-	"github.com/ovh/venom/executors/dbfixtures"
-	"github.com/ovh/venom/executors/exec"
-	"github.com/ovh/venom/executors/grpc"
-	"github.com/ovh/venom/executors/http"
-	"github.com/ovh/venom/executors/imap"
-	"github.com/ovh/venom/executors/kafka"
-	"github.com/ovh/venom/executors/mqtt"
-	"github.com/ovh/venom/executors/ovhapi"
-	"github.com/ovh/venom/executors/rabbitmq"
-	"github.com/ovh/venom/executors/readfile"
-	"github.com/ovh/venom/executors/redis"
-	"github.com/ovh/venom/executors/smtp"
-	"github.com/ovh/venom/executors/sql"
-	"github.com/ovh/venom/executors/ssh"
-	"github.com/ovh/venom/executors/web"
+	"github.com/ovh/venom/executors"
 )
 
 var (
@@ -321,22 +306,9 @@ var Cmd = &cobra.Command{
 		}
 
 		v = venom.New()
-		v.RegisterExecutorBuiltin(amqp.Name, amqp.New())
-		v.RegisterExecutorBuiltin(dbfixtures.Name, dbfixtures.New())
-		v.RegisterExecutorBuiltin(exec.Name, exec.New())
-		v.RegisterExecutorBuiltin(grpc.Name, grpc.New())
-		v.RegisterExecutorBuiltin(http.Name, http.New())
-		v.RegisterExecutorBuiltin(imap.Name, imap.New())
-		v.RegisterExecutorBuiltin(kafka.Name, kafka.New())
-		v.RegisterExecutorBuiltin(mqtt.Name, mqtt.New())
-		v.RegisterExecutorBuiltin(ovhapi.Name, ovhapi.New())
-		v.RegisterExecutorBuiltin(rabbitmq.Name, rabbitmq.New())
-		v.RegisterExecutorBuiltin(readfile.Name, readfile.New())
-		v.RegisterExecutorBuiltin(redis.Name, redis.New())
-		v.RegisterExecutorBuiltin(smtp.Name, smtp.New())
-		v.RegisterExecutorBuiltin(sql.Name, sql.New())
-		v.RegisterExecutorBuiltin(ssh.Name, ssh.New())
-		v.RegisterExecutorBuiltin(web.Name, web.New())
+		for name, executorFunc := range executors.Registry {
+			v.RegisterExecutorBuiltin(name, executorFunc())
+		}
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		initArgs(cmd)
---
+++ executors/registry.go
@@ -0,0 +1,43 @@
+package executors
+
+import (
+	"github.com/ovh/venom"
+	"github.com/ovh/venom/executors/amqp"
+	"github.com/ovh/venom/executors/dbfixtures"
+	"github.com/ovh/venom/executors/exec"
+	"github.com/ovh/venom/executors/grpc"
+	"github.com/ovh/venom/executors/http"
+	"github.com/ovh/venom/executors/imap"
+	"github.com/ovh/venom/executors/kafka"
+	"github.com/ovh/venom/executors/mqtt"
+	"github.com/ovh/venom/executors/ovhapi"
+	"github.com/ovh/venom/executors/rabbitmq"
+	"github.com/ovh/venom/executors/readfile"
+	"github.com/ovh/venom/executors/redis"
+	"github.com/ovh/venom/executors/smtp"
+	"github.com/ovh/venom/executors/sql"
+	"github.com/ovh/venom/executors/ssh"
+	"github.com/ovh/venom/executors/web"
+)
+
+type Constructor func() venom.Executor
+
+// Registry is a map of executors to executor constructor functions.
+var Registry map[string]Constructor = map[string]Constructor{
+	amqp.Name:       amqp.New,
+	dbfixtures.Name: dbfixtures.New,
+	exec.Name:       exec.New,
+	grpc.Name:       grpc.New,
+	http.Name:       http.New,
+	imap.Name:       imap.New,
+	kafka.Name:      kafka.New,
+	mqtt.Name:       mqtt.New,
+	ovhapi.Name:     ovhapi.New,
+	rabbitmq.Name:   rabbitmq.New,
+	readfile.Name:   readfile.New,
+	redis.Name:      redis.New,
+	smtp.Name:       smtp.New,
+	sql.Name:        sql.New,
+	ssh.Name:        ssh.New,
+	web.Name:        web.New,
+}
