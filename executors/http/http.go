3ee08053d1dea8e255bdea35ced83180248aaf2b
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2022-08-23 10:13:35 +0200

    fix(executor/http): preserveBodyFile interpolation
    close #574
    
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- executors/http/http.go
+++ executors/http/http.go
@@ -238,7 +238,10 @@ func (Executor) Run(ctx context.Context, step venom.TestStep) (interface{}, erro
 	}
 
 	requestContentType := r.Request.Header.Get("Content-Type")
-	if !isContentTypeSupported(requestContentType) {
+	// if PreserveBodyFile == true, the body is not interpolated.
+	// So, no need to keep it in request here (to re-inject it in vars)
+	// this will avoid to by interpoloated after in vars too.
+	if e.PreserveBodyFile || !isContentTypeSupported(requestContentType) {
 		r.Request.Body = ""
 	}
 
