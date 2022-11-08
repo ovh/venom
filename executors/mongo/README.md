9173a1d3e633e63ce0ef15b7205236d86cc5b706
Thibaut Rousseau <thibaut.rousseau.44@gmail.com>
2022-11-08 16:23:20 +0100

    add mongo executor
    Signed-off-by: Thibaut Rousseau <thibaut.rousseau.44@gmail.com>

---
+++ executors/mongo/README.md
@@ -0,0 +1,199 @@
+# Venom - Executor Mongo
+
+Step to execute actions on a MongoDB database.
+
+## Input
+
+See also [tests/mongo.yml](../../tests/mongo.yml) for executable examples.
+
+Note: most fields support [MongoDB Extended JSON v2](https://www.mongodb.com/docs/manual/reference/mongodb-extended-json/).
+This means some special values (ObjectIds, ISODates, etc.) can be represented using the relaxed format.
+
+Example:
+
+```json
+{
+  "_id": {
+    "$oid": "5d505646cf6d4fe581014ab2"
+  }
+}
+```
+
+### Insert documents
+
+```yaml
+- type: mongo
+  uri: mongodb://localhost:27017
+  database: my-database
+  collection: my-collection
+  actions:
+    - type: insert
+      documents:
+        - '{"suit": "hearts", "value": "queen"}'
+        - '{"suit": "diamonds", "value": "three"}'
+```
+
+### Insert documents from a file
+
+```yaml
+- type: mongo
+  uri: mongodb://localhost:27017
+  database: my-database
+  collection: my-collection
+  actions:
+    - type: insert
+      file: fixtures/my-collection.jsonlist
+```
+
+`fixtures/my-collection.jsonlist`:
+
+```json
+{
+  "suit": "hearts",
+  "value": "queen"
+}
+{
+  "suit": "diamonds",
+  "value": "three"
+}
+```
+
+### Find documents
+
+```yaml
+- type: mongo
+  uri: mongodb://localhost:27017
+  database: my-database
+  collection: my-collection
+  actions:
+    - type: find
+      filter: |
+        {
+          "suit": "clubs"
+        }
+      options: # optional
+        limit: 3
+        skip: 1
+        sort: '{"_id": -1}'
+        projection: '{"value": 1}'
+```
+
+### Find documents by ObjectID
+
+See: [MongoDB Extended JSON - Type Representations](https://www.mongodb.com/docs/manual/reference/mongodb-extended-json/#type-representations)
+
+```yaml
+- type: mongo
+  uri: mongodb://localhost:27017
+  database: my-database
+  collection: my-collection
+  actions:
+    - type: find
+      filter: |
+        {
+          "_id": {
+            "$oid": "5d505646cf6d4fe581014ab2"
+          }
+        }
+      options: # optional
+        limit: 3
+        skip: 1
+        sort: '{"_id": -1}'
+        projection: '{"value": 1}'
+```
+
+### Count documents
+
+```yaml
+- type: mongo
+  uri: mongodb://localhost:27017
+  database: my-database
+  collection: my-collection
+  actions:
+    - type: count
+      filter: |
+        {
+          "suit": "clubs"
+        }
+```
+
+### Update documents
+
+```yaml
+- type: mongo
+  uri: mongodb://localhost:27017
+  database: my-database
+  collection: my-collection
+  actions:
+    - type: update
+      filter: |
+        {
+          "suit": {
+            "$in": ["clubs", "spades"]
+          }
+        }
+      update: |
+        {
+          "$set": {
+            "color": "black"
+          }
+        }
+```
+
+### Delete documents
+
+```yaml
+- type: mongo
+  uri: mongodb://localhost:27017
+  database: my-database
+  collection: my-collection
+  actions:
+    - type: delete
+      filter: |
+        {
+          "suit": "circles"
+        }
+```
+
+### Aggregate documents
+
+```yaml
+- type: mongo
+  uri: mongodb://localhost:27017
+  database: my-database
+  collection: my-collection
+  actions:
+    - type: aggregate
+      pipeline:
+        - |
+          { "$match": {
+            "color": "black"
+          }}
+        - |
+          { "$group": {
+            "_id": "$value",
+            "count": {"$sum": 1}
+          }}
+```
+
+### Create a collection
+
+```yaml
+- type: mongo
+  uri: mongodb://localhost:27017
+  database: my-database
+  collection: my-collection
+  actions:
+    - type: createCollection
+```
+
+### Drop a collection
+
+```yaml
+- type: mongo
+  uri: mongodb://localhost:27017
+  database: my-database
+  collection: my-collection
+  actions:
+    - type: dropCollection
+```
---
+++ executors/mongo/mongo.go
@@ -0,0 +1,327 @@
+package mongo
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/ovh/venom"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const Name = "mongo"
+
+func New() venom.Executor {
+	return &Executor{}
+}
+
+type Executor struct {
+	URI        string           `json:"uri,omitempty" yaml:"uri,omitempty"`
+	Database   string           `json:"database,omitempty" yaml:"database,omitempty"`
+	Collection string           `json:"collection,omitempty" yaml:"collection,omitempty"`
+	Actions    []map[string]any `json:"actions,omitempty" yaml:"actions,omitempty"`
+}
+
+type Result struct {
+	Actions []map[string]any `json:"actions,omitempty" yaml:"actions,omitempty"`
+}
+
+func (e Executor) Run(ctx context.Context, step venom.TestStep) (any, error) {
+	if err := mapstructure.Decode(step, &e); err != nil {
+		return nil, err
+	}
+
+	venom.Debug(ctx, "connecting to database: %s\n", e.URI)
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(e.URI))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+	defer mongoClient.Disconnect(ctx)
+
+	results := make([]map[string]any, len(e.Actions))
+
+	for i, action := range e.Actions {
+		actionType := fmt.Sprintf("%v", action["type"])
+		switch actionType {
+		case "dropCollection":
+			if err := mongoClient.Database(e.Database).Collection(e.Collection).Drop(ctx); err != nil {
+				return nil, err
+			}
+
+		case "createCollection":
+			if err := mongoClient.Database(e.Database).CreateCollection(ctx, e.Collection); err != nil {
+				return nil, err
+			}
+
+		case "count":
+			var countAction CountAction
+			if err := mapstructure.Decode(action, &countAction); err != nil {
+				return nil, err
+			}
+
+			filter := bson.M{}
+			if countAction.Filter != "" {
+				if err := bson.UnmarshalExtJSON([]byte(countAction.Filter), false, &filter); err != nil {
+					return nil, err
+				}
+			}
+
+			var countOptions []*options.CountOptions
+			if countAction.Options.Limit != nil {
+				countOptions = append(countOptions, options.Count().SetLimit(*countAction.Options.Limit))
+			}
+
+			count, err := mongoClient.Database(e.Database).Collection(e.Collection).CountDocuments(ctx, filter, countOptions...)
+			if err != nil {
+				return nil, err
+			}
+
+			results[i] = map[string]any{"count": count}
+
+		case "insert":
+			var insertAction InsertAction
+			if err := mapstructure.Decode(action, &insertAction); err != nil {
+				return nil, err
+			}
+
+			var documents []any
+
+			if insertAction.File != "" {
+				filePath := path.Join(venom.StringVarFromCtx(ctx, "venom.testsuite.workdir"), insertAction.File)
+				file, err := os.Open(filePath)
+				if err != nil {
+					return nil, err
+				}
+
+				decoder := json.NewDecoder(file)
+				for {
+					var documentJSON any
+					if err := decoder.Decode(&documentJSON); err != nil {
+						if err == io.EOF {
+							break
+						}
+						return nil, err
+					}
+
+					documentBytes, err := json.Marshal(documentJSON)
+					if err != nil {
+						return nil, err
+					}
+
+					var document bson.M
+					if err := bson.UnmarshalExtJSON(documentBytes, false, &document); err != nil {
+						return nil, err
+					}
+					documents = append(documents, document)
+				}
+			}
+
+			for _, documentJSON := range insertAction.Documents {
+				var document bson.M
+				if err := bson.UnmarshalExtJSON([]byte(documentJSON), false, &document); err != nil {
+					return nil, err
+				}
+				documents = append(documents, document)
+			}
+
+			insertManyResult, err := mongoClient.Database(e.Database).Collection(e.Collection).InsertMany(ctx, documents)
+			if err != nil {
+				return nil, err
+			}
+
+			var result map[string]any
+			if err := mapstructure.Decode(insertManyResult, &result); err != nil {
+				return nil, err
+			}
+			results[i] = result
+
+		case "find":
+			var findAction FindAction
+			if err := mapstructure.Decode(action, &findAction); err != nil {
+				return nil, err
+			}
+
+			filter := bson.M{}
+			if findAction.Filter != "" {
+				if err := bson.UnmarshalExtJSON([]byte(findAction.Filter), false, &filter); err != nil {
+					return nil, err
+				}
+			}
+
+			var findOptions []*options.FindOptions
+			if findAction.Options.Limit != nil {
+				findOptions = append(findOptions, options.Find().SetLimit(*findAction.Options.Limit))
+			}
+			if findAction.Options.Skip != nil {
+				findOptions = append(findOptions, options.Find().SetSkip(*findAction.Options.Skip))
+			}
+			if findAction.Options.Sort != "" {
+				var sort bson.M
+				if err := bson.UnmarshalExtJSON([]byte(findAction.Options.Sort), false, &sort); err != nil {
+					return nil, err
+				}
+				findOptions = append(findOptions, options.Find().SetSort(sort))
+			}
+			if findAction.Options.Projection != "" {
+				var projection bson.M
+				if err := bson.UnmarshalExtJSON([]byte(findAction.Options.Projection), false, &projection); err != nil {
+					return nil, err
+				}
+				findOptions = append(findOptions, options.Find().SetSort(projection))
+			}
+
+			cursor, err := mongoClient.Database(e.Database).Collection(e.Collection).Find(ctx, filter, findOptions...)
+			if err != nil {
+				return nil, err
+			}
+
+			var result []map[string]any
+			if err := cursor.All(ctx, &result); err != nil {
+				return nil, err
+			}
+			results[i] = map[string]any{"results": result}
+
+		case "update":
+			var updateAction UpdateAction
+			if err := mapstructure.Decode(action, &updateAction); err != nil {
+				return nil, err
+			}
+
+			filter := bson.M{}
+			if updateAction.Filter != "" {
+				if err := bson.UnmarshalExtJSON([]byte(updateAction.Filter), false, &filter); err != nil {
+					return nil, err
+				}
+			}
+
+			update := bson.M{}
+			if updateAction.Update != "" {
+				if err := bson.UnmarshalExtJSON([]byte(updateAction.Update), false, &update); err != nil {
+					return nil, err
+				}
+			}
+
+			var updateOptions []*options.UpdateOptions
+			if updateAction.Options.Upsert != nil {
+				updateOptions = append(updateOptions, options.Update().SetUpsert(*updateAction.Options.Upsert))
+			}
+
+			updateResult, err := mongoClient.Database(e.Database).Collection(e.Collection).UpdateMany(ctx, filter, update, updateOptions...)
+			if err != nil {
+				return nil, err
+			}
+
+			var result map[string]any
+			if err := mapstructure.Decode(updateResult, &result); err != nil {
+				return nil, err
+			}
+			results[i] = result
+
+		case "delete":
+			var deleteAction DeleteAction
+			if err := mapstructure.Decode(action, &deleteAction); err != nil {
+				return nil, err
+			}
+
+			filter := bson.M{}
+			if deleteAction.Filter != "" {
+				if err := bson.UnmarshalExtJSON([]byte(deleteAction.Filter), false, &filter); err != nil {
+					return nil, err
+				}
+			}
+
+			deleteResult, err := mongoClient.Database(e.Database).Collection(e.Collection).DeleteMany(ctx, filter)
+			if err != nil {
+				return nil, err
+			}
+
+			var result map[string]any
+			if err := mapstructure.Decode(deleteResult, &result); err != nil {
+				return nil, err
+			}
+			results[i] = result
+
+		case "aggregate":
+			var aggregateAction AggregateAction
+			if err := mapstructure.Decode(action, &aggregateAction); err != nil {
+				return nil, err
+			}
+
+			pipeline := bson.A{}
+			for _, pipelineItemJSON := range aggregateAction.Pipeline {
+				var pipelineItem bson.M
+				if err := bson.UnmarshalExtJSON([]byte(pipelineItemJSON), false, &pipelineItem); err != nil {
+					return nil, err
+				}
+				pipeline = append(pipeline, pipelineItem)
+			}
+
+			cursor, err := mongoClient.Database(e.Database).Collection(e.Collection).Aggregate(ctx, pipeline)
+			if err != nil {
+				return nil, err
+			}
+
+			var result []map[string]any
+			if err := cursor.All(ctx, &result); err != nil {
+				return nil, err
+			}
+			results[i] = map[string]any{"results": result}
+		}
+	}
+
+	return Result{Actions: results}, nil
+}
+
+type Action struct {
+	Type string
+}
+
+type CountAction struct {
+	Action
+	Filter  string
+	Options struct {
+		Limit *int64
+	}
+}
+
+type InsertAction struct {
+	Action
+	File      string
+	Documents []string
+}
+
+type FindAction struct {
+	Action
+	Filter  string
+	Options struct {
+		Limit      *int64
+		Skip       *int64
+		Sort       string
+		Projection string
+	}
+}
+
+type UpdateAction struct {
+	Action
+	Filter  string
+	Update  string
+	Options struct {
+		Upsert *bool
+	}
+}
+
+type DeleteAction struct {
+	Action
+	Filter string
+}
+
+type AggregateAction struct {
+	Action
+	Pipeline []string
+}
