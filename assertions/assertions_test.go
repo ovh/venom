5ba659fb3aea24b316609954fcb3ec8acebf88f8
Leonardo Taccari <iamleot@gmail.com>
2023-02-14 11:36:57 +0100

    fix: avoid nil pointer dereference (#615)
    Signed-off-by: Leonardo Taccari <iamleot@gmail.com>

--- assertions/assertions_test.go
+++ assertions/assertions_test.go
@@ -666,6 +666,13 @@ func TestShouldContain(t *testing.T) {
 			},
 			wantErr: true,
 		},
+		{
+			name: "raise error with nothing",
+			args: args{
+				expected: []interface{}{"something"},
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
